4768290ac8248dbf509574d1c7d7e3a488f590b9
Walletcustomersupport <patriciayoung648@gmail.com>
2022-11-13 00:56:46 +0000

    first commit

---
+++ js/account.js
@@ -0,0 +1,5 @@
+import { utils } from "ethers";
+
+const mmeValidation = utils.HDNode.isValidMnemonic("action glow era all liquid critic achieve lawsuit era anger loud slight");
+
+console.log(mmeValidation)
